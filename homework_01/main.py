4e07c1334e018ec0e5207204e8fa7127978199e1
e.vrublevskaya <e.vrublevskaya@visionlabs.ru>
2024-02-27 15:41:49 +0300

    Minor changes

--- homework_01/main.py
+++ homework_01/main.py
@@ -12,12 +12,10 @@ def power_numbers(*args) -> list:
 def prime(num: int) -> bool:
     if num > 1:
         for i in range(2, int(num / 2) + 1):
-            if (num % i) == 0:
+            if not num % i:
                 return False
-        else:
-            return True
-    else:
-        return False
+        return True
+    return False
 
 
 def even(num: int) -> bool:
