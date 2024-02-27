1851b86776fec7911096755c83305651fd3d6feb
e.vrublevskaya <e.vrublevskaya@visionlabs.ru>
2024-02-27 15:39:50 +0300

    Minor changes

--- homework_01/main.py
+++ homework_01/main.py
@@ -2,13 +2,14 @@
 Домашнее задание №1
 Функции и структуры данных
 """
+from typing import Callable
 
 
-def power_numbers(*args):
+def power_numbers(*args) -> list:
     return [arg ** 2 for arg in args]
 
 
-def prime(num):
+def prime(num: int) -> bool:
     if num > 1:
         for i in range(2, int(num / 2) + 1):
             if (num % i) == 0:
@@ -19,11 +20,11 @@ def prime(num):
         return False
 
 
-def even(num):
+def even(num: int) -> bool:
     return not num % 2
 
 
-def odd(num):
+def odd(num: int) -> int:
     return num % 2
 
 
@@ -32,5 +33,5 @@ EVEN = even
 PRIME = prime
 
 
-def filter_numbers(numbers, num_filter):
+def filter_numbers(numbers: list, num_filter: Callable):
     return list(filter(lambda x: num_filter(x), numbers))
