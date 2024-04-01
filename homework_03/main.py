9014941473e2d6e9fad266e99db7131f248a84b0
e.vrublevskaya <e.vrublevskaya@visionlabs.ru>
2024-04-01 17:47:24 +0300

    Main updated

--- homework_03/main.py
+++ homework_03/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get('/ping/', status_code=200)
 async def get_ping():
     return {"message": "pong"}
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='0.0.0.0', port=8000)
