dfe8c6f37e70b3f20f3e6a1257910cd0652e1f57
e.vrublevskaya <e.vrublevskaya@visionlabs.ru>
2024-04-01 17:57:14 +0300

    Main updated

--- homework_03/main.py
+++ homework_03/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
@@ -7,7 +8,8 @@ app = FastAPI()
 @app.get('/ping/', status_code=200)
 async def get_ping():
     return {"message": "pong"}
-
-
-if __name__ == '__main__':
-    uvicorn.run("main:app", host='0.0.0.0', port=8000)
+#
+#
+# if __name__ == '__main__':
+#     port = os.getenv('API_PORT', 8000)
+#     uvicorn.run("main:app", host='0.0.0.0', port=port)
